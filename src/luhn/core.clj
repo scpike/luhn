1793dd3edf6334805bec86d8a99c337b5794c8f5
Stephen Pike <spike@scpike.net>
2018-06-28 23:05:25 -0400

    Implement the luhn checksum algorithm

--- src/luhn/core.clj
+++ src/luhn/core.clj
@@ -1,6 +1,33 @@
-(ns luhn.core)
+; https://en.wikipedia.org/wiki/Luhn_algorithm
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(ns luhn.core
+  (:require [clojure.string :as str]))
+
+(defn string->digits
+  [s]
+  (map #(Integer/parseInt (str %)) (seq s)))
+
+; From the rightmost digit, which is the check digit, and moving left,
+; double the value of every second digit. The check digit is not
+; doubled, the first digit doubled is immediately to the left of the
+; check digit. If the result of this doubling operation is greater
+; than 9 (e.g., 8 × 2 = 16), then add the digits of the product (e.g.,
+; 16: 1 + 6 = 7, 18: 1 + 8 = 9) or, alternatively, the same result can
+; be found by subtracting 9 from the product (e.g., 16: 16 − 9 = 7,
+; 18: 18 − 9 = 9).
+(defn double-and-sum
+  [digits]
+  (->>
+   (map #(* 2 %) digits)
+   (map #(if (> % 9) (- % 9) %))))
+
+(defn luhn-checksum
+  [s]
+  (let [digits (reverse (string->digits s))
+        [to-double rest] (apply map list (partition 2 digits))]
+    (mod (* 9 (reduce + (flatten [(double-and-sum to-double) rest]))) 10)))
+
+(defn valid-luhn-checksum?
+  [s]
+  (= (luhn-checksum (drop-last s))
+     (Character/getNumericValue (last s))))
--- test/luhn/core_test.clj
+++ test/luhn/core_test.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [luhn.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-string->digits
+  (testing "Converts a string to digits."
+    (is (= [1 2 3] (string->digits "123")))))
+
+(deftest compute-checksum
+  (testing "Computes a checksum."
+    (is (= 3 (luhn.core/luhn-checksum "7992739871")))))
+
+(deftest checks-luhn
+  (testing "Validates luhn."
+    (is (= true (luhn.core/valid-luhn-checksum? "79927398713")))
+    (is (= false (luhn.core/valid-luhn-checksum? "79927398711")))))
